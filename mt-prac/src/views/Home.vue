5afc88f8b63308dabb1b1592c22cfe0ba8d52160
yxf <245417636@qq.com>
2019-08-23 18:19:58 +0800

    nochange too much

---
+++ mt-prac/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <header></header>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    // HelloWorld
+  }
+})
+export default class Home extends Vue {}
+</script>
+<style lang="less" scoped>
+.home {
+  header {
+    width: 100%;
+    height: 50px;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+}
+</style>
